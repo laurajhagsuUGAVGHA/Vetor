970288774747558536ceba4788f687d740d21b77
Laura <133364744+laurajhagsuUGAVGHA@users.noreply.github.com>
2024-05-10 16:31:42 -0300

    Initial commit

---
+++ Vetor/Vetor.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	////DECLARAR VETOR INTEIRO COM VALORES
+	//vector<int> numeros = { 10,20, 30, 40, 50 };
+
+	////ADICIONAR NOVOS ELEMENTOS AO VETOR
+	//numeros.push_back(60);
+	//numeros.push_back(70);
+
+	////MOSTRAR O VETOR
+	//for (int i = 0; i < numeros.size(); i++) {
+	//	cout << numeros[i];
+	//}
+
+
+	//vector<int> num(5);
+	////PREENCHENDO O VETOR PELO CONSOLE
+	//cout << "Digite 5 numeros para o vetor: ";
+
+	////INSERINDO ELEMENTOS NO VETOR
+	//for (int i = 0; i < 5; i++) {
+	//	cout << "Numero: ";
+	//	cin >> num[i];
+	//}
+	////MOSTRANDO  O VETOR QUE CRIAMOS
+	//for (int i = 0; i < num.size(); i++) {
+	//	cout << num[i];
+	//}
+
+//	//PROCURANDO UM NÚMERO NO VETOR
+//	vector<int> notas = { 10, 8, 7, 9, 5 };
+//
+//	//int numeroProcurando = 7;
+//	//bool encontrou = false;
+//	//for (int i = 0; i < notas.size(); i++) {
+//	//	if (notas[i] == numeroProcurando) {
+//	//		encontrou = true;
+//	//		break; //PARA O FOR 
+//	//	}
+//	//}
+//	//if (encontrou) {
+//	//	cout << "O numero foi encontrado";
+//	//}
+//	//else {
+//	//	cout << "O numero nao foi encontrado";
+//	//}
+//	////ORDENANDO O VETOR POR ORDEM CRESCENTE
+//	//sort(notas.begin(), notas.end());
+//	//for (int num = 0; num < notas.size(); num++) {
+//	//	cout << notas[num];
+//	//	cout << "\n";
+//	//}
+//	////ORDENANDO O VETOR POR ORDEM DECRESENTE
+//	//for (int i = 0; i < notas.size(); i++) {
+//	//	cout << notas[i];
+//	//	cout << "\n";
+//	//}
+//	double soma = 0;
+//	double media = 0;
+//
+////	//SOMANDO OS ELMENTOS DO VETOR
+//	for (int i = 0; i < notas.size(); i++) {
+//	soma += notas[i];}
+////	//CALCULANDO A MEDIA DOS ELEMNETOS
+//	media = soma / notas.size();
+//	cout << "Media: " << media;
+//	cout << "Media dos elementos de vetor: " << soma;
+//	cout << "Media dos elementos do vetor: " << media;
+//return 0;
+
+}
